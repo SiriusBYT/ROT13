a7e2715c7c89bc925913d5770c0ccd11b3b911e3
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2024-11-12 11:18:41 +0000

    barely fix shet

--- rot13.c
+++ rot13.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
+// Note: does not support caps lol and will segfault on command when provided nothing
 int main(int argc, char* argv[]) {
     char alphabet[27] = {"abcdefghijklmnopqrstuvwxyz\0"};
-    // printf("%s\n", alphabet);
 
     char* decrypted = argv[1];
     int text_length = sizeof(decrypted) / sizeof(decrypted[0]);
     char encrypted[text_length];
     int shift = 0;
-    printf("ROT13-ing '%s'...\n\n", decrypted);
 
     Encrypt:
+        printf("ROT13-ing '%s'...\n\n", decrypted);
         for (int cycle = 0; cycle < text_length; cycle++) {
             switch (decrypted[cycle]) {
             case '\0':
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
                 for (int subcycle = 0; subcycle < 26; subcycle++) {
                     if (decrypted[cycle] == alphabet[subcycle]) {
                         shift = subcycle+13;
-                        if (shift > 26) { shift-=26; }
+                        if (shift >= 26) { shift-=26; }
                         printf("Shifting letter %c (position %d) to letter %c (position %d)... \n", decrypted[cycle], subcycle, alphabet[shift], shift);
                         encrypted[cycle] = alphabet[shift];
                         break;
@@ -33,5 +33,5 @@ int main(int argc, char* argv[]) {
     Output:
         encrypted[text_length] = '\0';
         printf("ROT13-ified: '%s'\n", encrypted);
-        printf("%d\n", text_length);
+        return 0;
 }
