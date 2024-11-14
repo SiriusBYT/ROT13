2ded07029aafb6e6ea528168041b91e12021b84c
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2024-11-14 12:14:10 +0100

    stuff

--- rot13.c
+++ rot13.c
@@ -1,37 +1,87 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-// Note: does not support caps lol and will segfault on command when provided nothing
-int main(int argc, char* argv[]) {
-    char alphabet[27] = {"abcdefghijklmnopqrstuvwxyz\0"};
+const char Alphabet[27] = {"abcdefghijklmnopqrstuvwxyz\0"};
 
-    char* decrypted = argv[1];
-    int text_length = sizeof(decrypted) / sizeof(decrypted[0]);
-    char encrypted[text_length];
-    int shift = 0;
+void Help() {
+    printf("ROT13 v241114 - Usage\n");
+    printf("-h\n    Print this help message\n\n");
+    printf("-d [String]\n    Decrypt the string by shifting each letter by -13 letters.\n");
+    printf("-e [String]\n    Encrypt the string by shifting each letter by +13 letters.\n");
+    // printf("-s [int]\n   Shift by [int] letters instead of the default 13.\n"); I'm too lazy to do this
+}
 
-    Encrypt:
-        printf("ROT13-ing '%s'...\n\n", decrypted);
-        for (int cycle = 0; cycle < text_length; cycle++) {
-            switch (decrypted[cycle]) {
-            case '\0':
-                printf("NULL found, stopping! \n");
+// Note: does not support caps lol
+int main(int argc, char* argv[]) {    
+    // Avoid segmentation fault if there's nothing provided in the arguments
+    if (argc == 1) { Help(); return 0; }
+
+    // Handling arguments
+    int opt; int Shift = 13; int Letter;
+    bool Encrypt;
+    char* String; // This sounds stupid and might cause problems later on
+    while ((opt = getopt(argc, argv, "he:d:")) != -1) {
+        switch (opt) {
+            case 'h':
+                Help();
+                return 0;
                 break;
-            default:
-                for (int subcycle = 0; subcycle < 26; subcycle++) {
-                    if (decrypted[cycle] == alphabet[subcycle]) {
-                        shift = subcycle+13;
-                        if (shift >= 26) { shift-=26; }
-                        printf("Shifting letter %c (position %d) to letter %c (position %d)... \n", decrypted[cycle], subcycle, alphabet[shift], shift);
-                        encrypted[cycle] = alphabet[shift];
-                        break;
+            case 'd':
+                printf("Setting '%s' to be decrypted...\n", optarg);
+                String = optarg;
+                Encrypt = false;
+                break;
+            case 'e':
+                printf("Setting '%s' to be encrypted...\n", optarg);
+                String = optarg;
+                Encrypt = true;
+                break;
+            case ':':
+                printf("Missing argument for '%d'!\n", opt);
+            case '?':
+                printf("Unknown argument: '%d'\n", opt);
+                break;
+        }
+    }
+
+    int Displayed_Shift;
+    (Encrypt == true) ? (Displayed_Shift=13) : (Displayed_Shift=-13);
+    printf("Processing '%s' while shifting every character by '%d'...\n", String, Displayed_Shift);
+
+    int String_Length = sizeof(String) / sizeof(String[0]);
+    char Final[String_Length];
+
+    goto Process;
+
+    // Turn this idiot into a function later u lazy fuckhead
+    Process:
+        for (int cycle = 0; cycle < String_Length; cycle++) {
+            switch (String[cycle]) {
+                case '\0':
+                    // printf("NULL found, stopping! \n");
+                    goto Output;
+                default:
+                    for (int subcycle = 0; subcycle < 26; subcycle++) {
+                        if (String[cycle] == Alphabet[subcycle]) {
+                            // If we encrypt then we go [SHIFT] amounts forwards, otherwise go backwards.
+                            Letter = subcycle;
+                            (Encrypt == true) ? (Letter-=Shift) : (Letter-=Shift);
+                            // If OOB then readjust
+                            if (Letter >= 26) { Letter-=26; }
+                            if (Letter < 0 ) { Letter+=26; }
+                            //printf("Shifting letter %c (position %d) to letter %c (position %d)... \n", String[cycle], subcycle, Alphabet[Letter], Letter);
+                            Final[cycle] = Alphabet[Letter];
+                            break;
+                        }
                     }
+                    // printf("%c\n", Final[cycle]);
+                    break;
                 }
-                break;
-            }
         }
 
     Output:
-        encrypted[text_length] = '\0';
-        printf("ROT13-ified: '%s'\n", encrypted);
+        Final[String_Length] = '\0';
+        printf("Output: '%s'\n", Final);
         return 0;
 }
